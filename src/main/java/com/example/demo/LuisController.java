e9c8f54314f40691e45df8794b641e2456c37be8
Luis Pacheco <luis.pacheco@nubexx.es>
2023-01-11 16:04:14 +0100

    tercer

---
+++ src/main/java/com/example/demo/LuisController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LuisController {
+	@GetMapping("/check")
+	public String hola() {
+		return "estamos en la nubexx"; 		
+	}
+}
